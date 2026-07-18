0a34f701d07a4c4c6de6fc069b1c12631b58ac9f
art-ian <artainian.abulencia05@gmail.com>
2026-07-18 13:07:06 +0800

    Delete constants.php

--- constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('SITE_NAME', 'OWL Musical Instruments');
-define('GROUP_NAME', 'OWL');
-
-define('BASE_URL', 'http://localhost/owl-instruments/');
-
-// Used to build the email confirmation link
-define('SITE_EMAIL_FROM', 'no-reply@owlinstruments.local');
